9669f975e1847e4bf0168dc4ff157b0c992485e3
Szymon Miś <b2b@szymon-mis.pl>
2023-02-07 09:28:19 +0100

    ReadonlyDrawer

---
+++ Runtime/Core/Attributes/ReadOnlyAttribute.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace SMF.Core
+{
+	/// <summary>
+	/// Read Only attribute.
+	/// Attribute is use only to mark ReadOnly properties.
+	/// </summary>
+	public class ReadOnlyAttribute : PropertyAttribute { }
+}
