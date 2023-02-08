d1a46b18b789797cbd0e1a2cdbf977308e2db780
Szymon Miś <b2b@szymon-mis.pl>
2023-02-08 05:40:37 +0100

    Update TagAttribute.cs

--- Editor/Core/Attributes/TagAttribute.cs
+++ Editor/Core/Attributes/TagAttribute.cs
@@ -3,7 +3,8 @@ using System;
 namespace SMF.Editor.Core
 {
 	/// <summary>
-	/// 
+	/// Tag attribute.
+	/// Attribute is use to display and select one of tags. It changes string variable in the inspector to tag enum.
 	/// </summary>
 	[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
 	public class TagAttribute : Attribute { }
